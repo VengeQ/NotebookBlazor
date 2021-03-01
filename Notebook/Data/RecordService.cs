0657e0ebb9a35b849879661e5d73d60a025b5adf
Daniil B <vengeq@gmail.com>
2021-03-01 22:12:18 +1000

    Add project files.

---
+++ Notebook/Data/RecordService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notebook.Data
+{
+    public class RecordService
+    {
+        private List<Record> records;
+
+        public RecordService()
+        {
+            records = new List<Record>
+            {
+                new Record(1, "First"),
+                new Record(2, "Second"),
+                new Record(3, "Third")
+            };
+        }
+
+        public Task<IEnumerable<Record>> GetRecordsAsync()
+        {
+            return Task.FromResult(records.AsEnumerable());
+        }
+
+        public Task DeleteTaskAsync(uint filter)
+        {
+            return Task.Run(() => 
+                records = records
+                    .Where(record => record.Id != filter)
+                    .ToList()
+            );
+        }
+
+    }
+}
